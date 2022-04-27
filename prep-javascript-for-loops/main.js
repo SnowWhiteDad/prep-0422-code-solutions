312be12ee63062ba27ff4ab2a59daa06846c3d79
Ola Sh2 <shittu.babs@gmail.com>
2022-04-27 05:12:37 +0000

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,13 @@
+// create your loops here.
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (let j = 0; j < 10; j++) {
+  console.log(2 * j);
+}
+
+for (let m = 100; m > 0; m--) {
+  console.log('Time till explosion: ' + m);
+}
